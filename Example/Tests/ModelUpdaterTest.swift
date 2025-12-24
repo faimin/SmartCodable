8e2fc294c4db5ca08a85239b6fc3a54c7a6949c5
faimin <fuxianchao@gmail.com>
2025-12-24 14:59:18 +0800

    update combine test

--- Example/Tests/ModelUpdaterTest.swift
+++ Example/Tests/ModelUpdaterTest.swift
@@ -11,9 +11,11 @@ import Testing
 import Combine
 
 class ModelUpdaterTest: NSObject {
-    struct Model: SmartCodableX {
-        var name: String = ""
-        var age: Int = 0
+    class Model: ObservableObject, SmartCodableX {
+        @SmartPublished var name: String = ""
+        @SmartPublished var age: Int = 0
+        
+        required init() { }
     }
     
     @Published var model: Model?
@@ -23,11 +25,31 @@ class ModelUpdaterTest: NSObject {
     override init() {
         super.init()
         
+        // ✅
         $model
             .filter { $0 != nil }
             .removeDuplicates(by: { $0?.age == $1?.age })
             .sink { m in
                 print(">>>>> model 1 change = \(m?.age ?? -1)")
+            }
+            .store(in: &disposeBag)
+        
+        // ❌
+        model?.$age
+            .filter({ $0 != 0 })
+            .removeDuplicates()
+            .sink { x in
+                print(">>>>>> x = \(x)")
+            }
+            .store(in: &disposeBag)
+        
+        // ✅
+        $model
+            .compactMap({ $0?.age })
+            .filter({ $0 != 0 })
+            .removeDuplicates()
+            .sink { y in
+                print(">>>>>> y = \(y)")
             }
             .store(in: &disposeBag)
     }
@@ -51,5 +73,14 @@ class ModelUpdaterTest: NSObject {
         print(">>>>>> p1 = \(p1), p2 = \(p2)")
         
         #expect(model?.age == 200)
+        
+        model!.objectWillChange
+            .sink { [unowned self] _ in
+                print("对象属性即将发生变更")
+                #expect(model?.age == 200)
+            }
+            .store(in: &disposeBag)
+        
+        model?.age = 123
     }
 }
